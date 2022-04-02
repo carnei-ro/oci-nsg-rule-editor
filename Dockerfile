9783da30494ec60fce608cdd13d7c11528dd6ffe
Leandro Carneiro <leandro@carnei.ro>
2022-04-02 01:31:53 -0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM python:3 as downloader
+
+ARG oci_cli_version="3.7.0"
+
+RUN python -m venv /opt/venv
+
+ENV PATH="/opt/venv/bin:$PATH" \
+    OCI_CLI_VERSION="${oci_cli_version}"
+
+WORKDIR /
+
+RUN wget https://github.com/oracle/oci-cli/releases/download/v${OCI_CLI_VERSION}/oci-cli-${OCI_CLI_VERSION}.zip && \
+  unzip -j oci-cli-${OCI_CLI_VERSION}.zip oci-cli/oci_cli-${OCI_CLI_VERSION}-py3-none-any.whl && \
+  pip install oci_cli-${OCI_CLI_VERSION}-py3-none-any.whl
+
+FROM python:3 as ocicli
+
+COPY --from=downloader /opt/venv /opt/venv
+
+ENV PATH="/opt/venv/bin:$PATH"
+
+FROM ocicli as oci-nsg-rule-editor
+
+ENV OCI_CLI_AUTH="instance_principal"
+
+RUN apt update
+RUN apt install jq dnsutils -y
+
+COPY nsg-rule-editor.sh /
+
+ENTRYPOINT [ "/nsg-rule-editor.sh" ]
---
+++ nsg-rule-editor.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+export TOKEN="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
+export ENDPOINT="${ENDPOINT:-kubernetes.default.svc.cluster.local}"
+export NAMESPACE="${NAMESPACE:-default}"
+export CONFIGMAP_NAME="${CONFIGMAP_NAME:-raspberry-ip}"
+
+if [[ -z "${NAMESPACE}" ]] || [[ -z "${CONFIGMAP_NAME}" ]] || [[ -z "${FQDN}" ]] || [[ -z "${NSG_ID}" ]]; then
+  echo "Environment Variables NAMESPACE, CONFIGMAP_NAME, FQDN, NSG_ID are required."
+fi
+
+echo -e "Monitoring: \t ${FQDN}"
+echo -e "OCI NSG ID: \t ${NSG_ID}"
+echo -e "K8s Endpoint: \t ${ENDPOINT}"
+echo -e "ConfigMap Name: \t ${CONFIGMAP_NAME}"
+
+while sleep 60; do
+
+  OLD_IP_ADDR="$(curl -sk \
+      -H "Authorization: Bearer $TOKEN" \
+      -H 'Accept: application/json' \
+      https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/configmaps/$CONFIGMAP_NAME | jq -r .data.ipaddr)"
+
+  NEW_IP_ADDR=$(dig +short @8.8.8.8 A $FQDN)
+
+  if [[ "${OLD_IP_ADDR}" != "${NEW_IP_ADDR}" ]]; then
+    oci network nsg rules list --nsg-id "${NSG_ID}" | jq '.data | walk(if type == "object" then del(."is-valid", ."time-created") else . end) | {securityRules: ., nsgId: "__NSG_ID__"} ' | sed -e "s|__NSG_ID__|${NSG_ID}|g" -e "s|${OLD_IP_ADDR}|${NEW_IP_ADDR}|g"  | tee /tmp/update.json && \
+    oci network nsg rules update --from-json file:///tmp/update.json && \
+    curl -skX PATCH \
+      -H "Authorization: Bearer $TOKEN" \
+      -H 'Content-Type: application/strategic-merge-patch+json' \
+      https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/configmaps/$CONFIGMAP_NAME \
+      -d "{\"data\":{\"ipaddr\": \"${NEW_IP_ADDR}\"}}"
+  fi
+done
